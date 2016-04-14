6e11a695c51753e36311361bb224741965b004cf
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-04-14 11:14:34 +0200

    Dodanie testu "confirmMethodShouldThrowOrderExpiredException()"

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,28 @@
+package edu.iis.mto.time;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+import org.joda.time.DateTimeUtils;
+import org.joda.time.Duration;
+import org.junit.Test;
+
+public class OrderTest {
+
+	private long invalidDate = DateTimeUtils.currentTimeMillis() + Duration.standardDays(1).getMillis()*2;
+	
+	@Test (expected = OrderExpiredException.class)
+	public void confirmMethodShouldThrowOrderExpiredException() {
+		DateTimeUtils.setCurrentMillisFixed(System.currentTimeMillis());
+		Order order = new Order();
+		
+		
+		order.submit();
+		
+		DateTimeUtils.setCurrentMillisFixed(invalidDate);
+		
+		order.confirm();
+	}
+
+}
